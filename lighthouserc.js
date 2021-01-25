8b73c24a2fdfdae977423b6d47e60efcba7fe0b6
FCosta20 <fcosta@solvd.com>
2021-01-25 14:31:56 -0300

    modified lighthousecr

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,14 +1,5 @@
 module.exports = {
     ci: {
-        collect: {
-            numberOfRuns: 3,
-            staticDistDir: ".dist/lighthouse-ci"
-        },
-        assert: {
-            assertions: {
-                "first-contentful-paint": ["warn", {maxNumericValue: 6000}]
-            }
-        },
         upload: {
             target: "temporary-public-storage"
         }
